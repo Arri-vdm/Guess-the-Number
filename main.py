08da92ac1a4b41af02d34eee0150899d6dacb840
Armand van der Merwe <arri.vdm@gmail.com>
2022-01-28 14:06:51 +0000

    Version 1 - Completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # 1. Imports
 from art import logo
-import random
+from random import randint
 
 # 2. Print logo and Welcome
 print(logo)
@@ -10,26 +10,30 @@ print("\nWelcome to the Number Guessing Game!\n"
       "I'm thinking of a number between 1 and 100.")
 
 # 3. Generate a random number between 1 and 100
-answer = random.randint(1, 100)
+answer = randint(1, 100)
 
 # --->>> Testing:
-# print(f"Pssst, the correct answer is {answer}")
+print(f"Pssst, the correct answer is {answer}")
+
+# Global Variables
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 
 # 4. Define functions for operations
 # 4.1 Function 1
 def user_choice():
     """TAKES an input from the user:\n
-       --->> Choose a difficulty. Type 'easy' or 'hard'\n
-       - If 'easy' is chosen, user gets 10 turns.\n
-       - If 'hard' is chosen, user gets 5 turns.\n
-       --->> RETURNS: the number of turns\n
+    --->> Choose a difficulty. Type 'easy' or 'hard'\n
+    - If 'easy' is chosen, user gets 10 turns.\n
+    - If 'hard' is chosen, user gets 5 turns.\n
+    --->> RETURNS: the number of turns\n
     """
     easy_hard = input("\nChoose a difficulty. Type 'easy' or 'hard': ").lower()
     if easy_hard == "easy":
-        return 10
+        return EASY_LEVEL_TURNS
     elif easy_hard == "hard":
-        return 5
+        return HARD_LEVEL_TURNS
 
 
 # 4.2 Function 2
@@ -50,7 +54,7 @@ def guess():
     def guess_again(turns_left):
         """A NESTED FUNCTION):\n
             --->> Checks whether turns are equal or
-                  not equal to zero\n
+                not equal to zero\n
             - If zero turns prints out a message in correct          place\n
             - If not zero turns prints out a message in correct      place\n          
         """
@@ -59,31 +63,29 @@ def guess():
         elif turns == 0:
             print(f"\nYou've run out of guesses, you lose.\n\n")
 
-    if turns != 0:
-        while turns != 0:
-            # Prints remaining turns
-            print(
-                f"\nYou have {turns} attempts remaining to guess the number.")
-            # Get the guess from the user
-            guess = int(input("Make a guess: "))
-            # Serries of if, elif statements to
-            # compare the guess with the answer
-            if guess < 1 or guess > 100:
-                print(f"Invalid guess!\nIt should be between 1 and 100.")
-                turns -= 1
-            elif guess == answer:
-                print(f"\nYou got it! The answer was {answer}!\n\n")
-                turns = 0
-            elif guess > answer:
-                print(f"Too HIGH!")
-                turns -= 1
-                # Calls the guess again function
-                guess_again(turns_left=turns)
-            elif guess < answer:
-                print(f"Too LOW!")
-                turns -= 1
-                # Calls the guess again function
-                guess_again(turns_left=turns)
+    while turns != 0:
+        # Prints remaining turns
+        print(f"\nYou have {turns} attempts remaining to guess the number.")
+        # Get the guess from the user
+        guess = int(float(input("Make a guess: ")))
+        # Series of if, elif statements to
+        # compare the guess with the answer
+        if guess < 1 or guess > 100:
+            print(f"Invalid guess!\nIt should be between 1 and 100.")
+            turns -= 1
+        elif guess == answer:
+            print(f"\nYou got it! The answer was {answer}!\n\n")
+            turns = 0
+        elif guess > answer:
+            print(f"Too HIGH!")
+            turns -= 1
+            # Calls the guess again function
+            guess_again(turns_left=turns)
+        elif guess < answer:
+            print(f"Too LOW!")
+            turns -= 1
+            # Calls the guess again function
+            guess_again(turns_left=turns)
 
 
 # Call the MAIN function
